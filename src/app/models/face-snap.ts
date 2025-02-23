b6211cace610271ec71592355bc50a12365e2512
amdymstfa <amdymoustapha011@gmail.com>
2025-02-23 01:22:55 +0100

    done: creation of model, usage of class and contructor

---
+++ src/app/models/face-snap.ts
@@ -0,0 +1,11 @@
+export class faceSnap {
+    constructor (
+        public title : string,
+        public url : string,
+        public description : string,
+        public createAt : Date,
+        public snap : number,
+    ){
+
+    }
+}
