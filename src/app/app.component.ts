c4fe2f0486647cadb0afca4ecd3a6e8805f25c26
amdymstfa <amdymoustapha011@gmail.com>
2025-02-23 21:27:56 +0000

    done: component header for the header of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import {FaceSnapListComponent} from './face-snap-list/face-snap-list.component'
+import {FaceSnapListComponent} from './face-snap-list/face-snap-list.component';
+import {HeaderComponent} from './header/header.component'
+
 
 
 @Component({
   selector: 'app-root',
   imports: [
-    FaceSnapListComponent
+    FaceSnapListComponent,
+    HeaderComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
